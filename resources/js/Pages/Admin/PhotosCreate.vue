bab8edc3549bbfc35951bdb8cb179050f335177c
mkholidkamali <mohamadkholid.kamali@gmail.com>
2022-10-30 10:58:48 +0700

    [Update] Photos CRUD

---
+++ resources/js/Pages/Admin/PhotosCreate.vue
@@ -0,0 +1,24 @@
+<script>
+    import { defineComponent } from 'vue';
+    import AppLayout from '@/Layouts/AppLayout.vue';
+
+    export default defineComponent({
+        components: {
+            AppLayout
+        }
+    });
+</script>
+
+<template>
+    <AppLayout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Photos</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <h1 class="text-2xl">Add New Photo</h1>
+            </div>
+        </div>
+    </AppLayout>
+</template>
